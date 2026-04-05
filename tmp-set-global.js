a68dfedf0027be914b3f46c5ead1eabf5fe5b55d
ohsujee <yogarajah.sujeevan@gmail.com>
2026-04-05 21:39:28 +0200

    chore: remove temporary/unrelated files accidentally staged

--- tmp-set-global.js
+++
@@ -1,4 +0,0 @@
-const admin = require("firebase-admin");
-const sa = require("/opt/punkrecords-sa.json");
-if (!admin.apps.length) admin.initializeApp({ credential: admin.credential.cert(sa), databaseURL: "https://letsqueeze-default-rtdb.europe-west1.firebasedatabase.app" });
-admin.database().ref("config/forceUpdateVersion").set("0.0.0").then(() => { console.log("OK - global set to 0.0.0"); process.exit(0); });
